aa1373203067d2395263077127a39b42b409096c
Keremet Dogdurbaeva <dogdurbaeva04@bk.ru>
2022-09-19 16:33:32 +0600

    home work #3

--- Home work # 3/main.swift
+++ Home work # 3/main.swift
@@ -113,3 +113,105 @@ let secondNumber = Int(readLine()!)
 
 action(number: firstNumber!, number2: secondNumber!)
 
+
+
+
+
+
+
+// №1,2,3 задание/ классная работа
+
+//1
+func compp(num11: Int) {
+    if num11 % 2 == 0{
+        print("Четное")
+    }else{
+        print("Нечетное")
+       
+    }
+}
+print(compp(num11: 10))
+
+
+//2
+func com22(num90: Int){
+    if num90 > 10{
+       print("Больше 10")
+    }else{
+       print("Меньше 10")
+      
+    }
+}
+print(com22(num90: 23))
+
+
+//3
+
+var even: Int = 0
+var notEven: Int = 0
+var more10: Int = 0
+var less10: Int = 0
+
+func com1(num8: Int) {
+    if num8 % 2 == 0{
+//        print("Четное")
+        even += 1
+    }else{
+//        print("Нечетное")
+        notEven += 1
+    }
+}
+
+func com2(num9: Int){
+    if num9 > 10{
+//        print("Больше 10")
+        more10 += 1
+    }else{
+//        print("Меньше 10")
+        less10 += 1
+    }
+}
+
+
+if let i1 = readLine() {
+  if let num = Int(i1) {
+      com1(num8: num)
+      com2(num9: num)
+  }
+}
+if let i2 = readLine() {
+  if let num = Int(i2) {
+      com1(num8: num)
+      com2(num9: num)
+  }
+}
+if let i3 = readLine() {
+  if let num = Int(i3) {
+      com1(num8: num)
+      com2(num9: num)
+  }
+}
+if let i4 = readLine() {
+  if let num = Int(i4) {
+      com1(num8: num)
+      com2(num9: num)
+  }
+}
+if let i5 = readLine() {
+  if let num = Int(i5) {
+      com1(num8: num)
+      com2(num9: num)
+  }
+}
+
+print("Четные - \(even)")
+print("Нечетные - \(notEven)")
+print("Больше 10 - \(more10)")
+print("Меньше 10 - \(less10)")
+
+
+
+
+
+
+
